66d53efaadd6a4aa178a816c66aecedaade918fe
JoseOtavioZ <joseotz97@gmail.com>
2023-05-31 22:27:43 -0300

    Initial commit

---
+++ src/main/java/unipar/exemplo/connection/ConnectionGenerico.java
@@ -0,0 +1,11 @@
+package unipar.exemplo.connection;
+
+public class Connection {
+
+    private final String url="jdbc:post";
+    private final String username="";
+    private final String password ="";
+    private final String dbName="";
+
+
+}
---
+++ src/main/java/unipar/exemplo/repository/cliente/ReposirotyCliente.java
@@ -0,0 +1,2 @@
+package unipar.exemplo.repository.cliente;public class ReposirotyCliente {
+}
